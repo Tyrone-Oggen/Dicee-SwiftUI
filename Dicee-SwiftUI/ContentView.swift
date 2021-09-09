657ec2a58f01aa3a441a7e9fc50f6450d892fc8c
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-09-09 10:20:17 +0200

    Project init

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Tyrone Oggen on 2021/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
